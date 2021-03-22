bd7febb02be28fa86bde8aec88853fb3b28f21e8
sharknoise <sharknoise@gmail.com>
2021-03-22 22:01:33 +0300

    remove unused argument

--- tests/github_test.py
+++ tests/github_test.py
@@ -2,7 +2,7 @@ from page_load import core
 from tests import conftest
 
 
-def test_core(tmp_path, test_page):
+def test_core(tmp_path):
     core.download_page(conftest.TEST_URL, tmp_path)
     saved_page = tmp_path / conftest.TEST_PAGE_FILENAME
     assert saved_page.read_text() == conftest.TEST_PAGE_PATH.read_text()

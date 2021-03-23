f899df43020130727bb79a73ae7ba6775f157cfb
sharknoise <sharknoise@gmail.com>
2021-03-23 22:13:54 +0300

    rewrite tests with requests-mock

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,47 +2,34 @@
 from pathlib import Path
 
 import pytest
-import requests
+import requests_mock
 
 TESTS_DIR = Path(__file__).parent.absolute()
 FIXTURES_DIR = TESTS_DIR / 'fixtures'
-TEST_PAGE_FILENAME = 'sharknoise-github-io-index.html'
-TEST_PAGE_PATH = FIXTURES_DIR / TEST_PAGE_FILENAME
 TEST_URL = 'http://sharknoise.github.io/index.html'
-
-SUCCESSFUL = 200
-ERROR = 404
-
-class MockResponse:
-    def __init__(self, ok=True, text=None, status_code=SUCCESSFUL):
-        self.ok = ok
-        self.text = text
-        self.status_code = status_code
+EXPECTED_PAGE_FILENAME = 'sharknoise-github-io-index.html'
+FIXTURE_PAGE_PATH = FIXTURES_DIR / EXPECTED_PAGE_FILENAME
 
 
-@pytest.fixture
-def test_page():
-    page = {}
-
-    with open(TEST_PAGE_PATH) as f:
-        page['content'] = f.read()
-
-    page['filename'] = TEST_PAGE_FILENAME
-    page['url'] = TEST_URL
-    return page
+@pytest.fixture()
+def mock():
+    with requests_mock.Mocker(real_http=True) as m:
+        yield m
 
 
 @pytest.fixture
-def mock_response_successful(monkeypatch, test_page):
-    def mock_get(*args, **kwargs):
-        return MockResponse(text=test_page['content'])
-
-    monkeypatch.setattr(requests, 'get', mock_get)
+def mock_response200(mock):
+    mock.register_uri(
+        'GET',
+        TEST_URL,
+        text=FIXTURE_PAGE_PATH.read_text(),
+    )
 
 
 @pytest.fixture
-def mock_response_error(monkeypatch):
-    def mock_get(*args, **kwargs):
-        return MockResponse(ok=False, status_code=ERROR)
-
-    monkeypatch.setattr(requests, 'get', mock_get)
+def mock_response404(mock):
+    mock.register_uri(
+        'GET', 
+        TEST_URL,
+        status_code=404,
+    )

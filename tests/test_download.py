29a47d8a75d62753f20905cf7f22288288344e79
sharknoise <sharknoise@gmail.com>
2021-03-24 14:07:35 +0300

    fix TEMP_DIR

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,19 +3,41 @@ import pytest
 from page_load import core
 from tests import conftest
 
+TEMP_DIR = 'success'
+
 
 def test_core_real_response(tmp_path):
-    core.download_page(conftest.TEST_URL, tmp_path)
-    saved_page = tmp_path / conftest.EXPECTED_PAGE_FILENAME
-    assert saved_page.read_text() == conftest.FIXTURE_PAGE_PATH.read_text()
+    destination = tmp_path / TEMP_DIR
+    destination.mkdir()
+
+    core.download_page(conftest.TEST_URL, destination)
+    saved_page_path = destination / conftest.EXPECTED_PAGE_FILENAME
+
+    assert saved_page_path.exists(), (
+        "Wrong page filename or the page wasn't saved."
+    )
+
+    assert saved_page_path.read_text() == conftest.FIXTURE_PAGE_PATH.read_text(), (
+        'Content of the saved page is different.'
+    )
 
 
-def test_core_success(mock_response200, tmp_path):
-    core.download_page(conftest.TEST_URL, tmp_path)
-    saved_page = tmp_path / conftest.EXPECTED_PAGE_FILENAME
-    assert saved_page.read_text() == conftest.FIXTURE_PAGE_PATH.read_text()
+def test_core_mock_success(mock_response200, tmp_path):
+    destination = tmp_path / TEMP_DIR
+    destination.mkdir()
+
+    core.download_page(conftest.TEST_URL, destination)
+    saved_page_path = destination / conftest.EXPECTED_PAGE_FILENAME
+
+    assert saved_page_path.exists(), (
+        "Wrong page filename or the page wasn't saved."
+    )
+
+    assert saved_page_path.read_text() == conftest.FIXTURE_PAGE_PATH.read_text(), (
+        'Content of the saved page is different.'
+    )
 
 
-def test_core_fail(mock_response404):
+def test_core_mock_fail(mock_response404):
     with pytest.raises(ValueError):
         core.download_page(conftest.TEST_URL)

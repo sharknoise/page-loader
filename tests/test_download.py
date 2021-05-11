465c3b95e89a90bcda8479b3319c21ac93ed6310
sharknoise <sharknoise@gmail.com>
2021-05-11 23:19:46 +0300

    fix a bug in mocking

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,16 +1,38 @@
 from importlib import reload
 
 import pytest
-
 from page_load import core
 from tests import conftest as ct
 
 TEMP_DIR = 'success'
 
 
-# test with a real response first and then with a mock response
-@pytest.mark.parametrize('response', [None, ct.mock_response200])
-def test_download_page(response, tmp_path):
+def test_mock_download_page(mock_response200, tmp_path):
+    destination = tmp_path / TEMP_DIR
+    destination.mkdir()
+
+    # to avoid incrementing names_counter for repeating filenames
+    reload(core)
+    core.download_page(ct.TEST_URL, destination)
+    saved_page_path = destination / ct.SAVED_FIXTURE_FILENAME
+
+    assert saved_page_path.exists(), (
+        "Wrong page filename or the page wasn't saved."
+    )
+
+    assert (
+        saved_page_path.read_text() == ct.SAVED_FIXTURE_PATH.read_text()
+    ), ('Content of the saved page is different.')
+
+    for saved_res, mock_res in zip(
+        ct.SAVED_RESOURCE_PATHS, ct.MOCK_RESOURCE_PATHS,
+    ):
+        saved_res_path = destination / ct.SAVED_RESOURCE_DIR / saved_res
+        mock_res_path = ct.FIXTURES_DIR / mock_res
+        assert saved_res_path.read_bytes() == mock_res_path.read_bytes()
+
+
+def test_real_download_page(tmp_path):
     destination = tmp_path / TEMP_DIR
     destination.mkdir()
 
